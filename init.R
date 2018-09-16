5c5aced5a542c3271dd2e2ecdf166b86dabf0052
vikas saini <16806956+saini3108@users.noreply.github.com>
2018-09-17 02:32:28 +0530

    Add files via upload

--- init.R
+++ init.R
@@ -12,11 +12,18 @@ require(gstat)
 require(wesanderson)
 require(nnet)
 require(randomForest)
-
+require(plotly)
+require(readr)
+require(xgboost)
+require(markdown)
+require(ggplot2)
+require(GGally)
+require(tableplot)
+require(VIM)
 # car, foreach, methods, plyr, nlme, reshape2, stats, stats4, utils, grDevices
 
 
-# Not all of these are required but shinyapps.io was crashing and 
+# Not all of these are required but shinyapps.io was crashing and
 # importing one of these solved the issue
 require(kernlab)
 require(klaR)
@@ -33,21 +40,22 @@ require(party)
 require(MLmetrics)
 require(Cubist)
 require(testthat)
-
+require(corrplot)
 
 data(meuse)
 
 dmnds <- diamonds#[sample(1:nrow(diamonds),1e3),]
 
-# leaf <- read.csv('/Users/davesteps/Desktop/kaggle_data/leaf/train.csv')
+
+titanic <- read.csv('train.csv')
 
 datasets <- list(
   'iris'=iris,
+  'titanic'=titanic,
   'cars'=mtcars,
   'meuse'=meuse,
   'diamonds'=data.frame(dmnds),
-  'Boston'=Boston
-  # 'leaf'=leaf
+  'Boston'= Boston
   # 'midwest'=data.frame(midwest),
   # 'mpg'=data.frame(mpg),
   # 'msleep'=data.frame(msleep),
@@ -55,36 +63,43 @@ datasets <- list(
 )
 
 tuneParams <- list(
-  'svmLinear'=data.frame(C=c(0.01,0.1,1)),
+  'svmLinear'=data.frame(C=c(0,0.01,0.1,1)),
   'svmPoly'= expand.grid(degree=1:3,scale=c(0.01,0.1),C=c(0.25,0.5,1)),
+  'svmRadial' = expand.grid(sigma = c(0,0.01,0.05,0.1) , C = c(0.25,0.5,1)),
   'nnet'=expand.grid(size=c(1,3,5),decay=c(0.01,0.1,1)),
   'rf'=data.frame(mtry=c(2,3,4)),
   'knn'=data.frame(k=c(1,3,5,7,9)),
   'nb'=expand.grid(usekernel=c(T,F),adjust=c(0.01,0.1,1),fL=c(0.01,0.1,1)),
-  'glm'=NULL#data.frame()
+  'glm'=NULL,
+  'gam' = NULL,
+  #'gbm' = NULL,
+  'xgbLinear' = NULL
 )
 
 
 mdls <- list('svmLinear'='svmLinear',
              'svmPoly'='svmPoly',
+             'svmRadial' = 'svmRadial',
              'Neural Network'='nnet',
              'randomForest'='rf',
              'k-NN'='knn',
              'Naive Bayes'='nb',
              'GLM'='glm',
-             'GAM'='gam')
+             'GAM'='gam',
+             #'GBM' = 'gbm',
+             'xgbLinear' = 'xgbLinear')
 #multinom
 
 mdli <- list(
-  'Regression'=c(T,T,T,T,T,F,T,F),
-  'Classification'=c(T,T,T,T,T,T,F,F)
-)  
+  'Regression'=c(T,T,T,T,T,T,F,T,F,F,F),
+  'Classification'=c(T,T,T,F,T,T,T,F,F,F,F)
+)
 
 reg.mdls <- mdls[mdli[['Regression']]]
 cls.mdls <- mdls[mdli[['Classification']]]
 
 
-# 
+#
 pal <- c('#b2df8a','#33a02c','#ff7f00','#cab2d6','#b15928',
          '#fdbf6f','#a6cee3','#fb9a99','#1f78b4','#e31a1c')
 set.seed(3)
@@ -99,7 +114,7 @@ modelCSS <-   function(item,col){
 tableCSS <- function(model,col){
   paste0('if (data[6] == "',model,'")
          $("td", row).css("background", "',col,'");')
-}  
+}
 
 label.help <- function(label,id){
   HTML(paste0(label,actionLink(id,label=NULL,icon=icon('question-circle'))))
